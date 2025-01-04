9bb42f3c6153dcac2b316b1a5f2ab8280a3b62fa
Vineeth Narmala <n.vineeth1704@gmail.com>
2025-01-04 13:13:56 -0500

    add image transition

--- QRcodeGenerator/Qr.js
+++ QRcodeGenerator/Qr.js
@@ -1,4 +1,6 @@
 const button = document.querySelector("#bt1");
+const inputBox = document.querySelector(".qrCode input");
+const qrimage=document.querySelector(".qrimage");
 
 async function QrCode(data) {
   const qrCodeUrl = "https://api.qrserver.com/v1/create-qr-code/";
@@ -15,23 +17,30 @@ async function QrCode(data) {
     if (!response.ok) {
       // const encodeData=encodeURIComponent(data);
       throw new Error(`error:${response.status}`);
+      inputBox.classList.add("shake");
+      qrimage.classList.remove("urlImg");
+      
     }
     const blob = await response.blob();
     const imageURl = URL.createObjectURL(blob);
-    const qrimage=document.querySelector(".qrimage");
+   
     const img = document.createElement("img");
-      img.src = imageURl;
+      qrimage.classList.add("urlImg");
+       img.src = imageURl;
        qrimage.appendChild(img);
-
+       inputBox.classList.remove("shake");
     console.log(`QrCode genrated sucessfully`);
   } catch (error) {
+    qrimage.classList.remove("urlImg");
+    inputBox.classList.add("shake");
+    setTimeout(()=>{
+      inputBox.classList.remove("shake");
+    },2000);
     console.error(`failure to genrate QrCode`);
   }
 }
 
 button.addEventListener("click", () => {
-  const inputBox = document.querySelector(".qrCode input").value;
-  
-    QrCode(inputBox);
-   
+  let value=inputBox.value;
+    QrCode(value);
 });
